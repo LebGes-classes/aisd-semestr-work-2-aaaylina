7ae7ce7db481ef5f3662717c5ca4094729024338
aaaylina <aylina.fedorova@mail.ru>
2025-06-12 17:35:10 +0300

    Add files via upload

---
+++ bucket_sort.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+//Сортировка вставками 
+void insertionSort(vector<double>& bucket) {
+	int n = bucket.size();
+	for (int i = 1; i < n; i++) {
+		double key = bucket[i];//текущий элемент для вставки
+		int j = i - 1;
+
+		//сдвигаем элементы больше key вправо
+		while (j >= 0 && bucket[j] > key) {
+			bucket[j + 1] = bucket[j];
+			j--;
+		}
+		bucket[j + 1] = key;
+
+	}
+}
+
+//bucket_sort
+void bucketSort(vector<double>& arr) {
+	
+	//поиск минимуму и максимума
+	double min = arr[0];
+	double max = arr[0];
+	for (double num : arr) {
+		if (num < min) {
+			min = num;
+		}
+		if (num > max) {
+			max = num;
+		}
+	}
+
+	//создание бакетов
+	int count = arr.size();
+	vector<vector<double>> buckets(count);
+
+	//распределение чисел по бакетам
+	for (double num : arr) {
+		double pos = (num - min) / (max - min); //(0...1)
+		int bucket_index = pos * count; //определяем индекс ведра
+
+		if (bucket_index >= count) {
+			bucket_index = count - 1; //защита от выхода за границы
+		}
+		buckets[bucket_index].push_back(num);
+	}
+
+	//сортировка каждого бакета
+	for (auto& bucket : buckets) {
+		insertionSort(bucket);
+	}
+
+	//сборка в исходный массив
+	int index = 0;
+	for (auto& bucket : buckets) {
+		for (double num : bucket) {
+			arr[index] = num;
+			index++;
+		}
+	}
+}
+int main() {
+	cout << "Введите числа: \n";
+
+	vector<double> numbers;
+	double num;
+	while (cin >> num) {
+		numbers.push_back(num);
+	}
+
+	bucketSort(numbers);
+
+	cout << "Отсортированный массив:\n";
+	for (double n : numbers) {
+		cout << n << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
---
+++ radix_sort.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> a;
+int n;
+
+//функция для получения максимального элемента
+int getMax() {
+	int max = a[0];
+	for (int i = 1; i < n; i++) {
+		if (a[i] > max) {
+			max = a[i];
+		}
+	}
+	return max;
+}
+
+//сортировка по конкретному разряду
+void sortByDigit(int r) {
+	int d10 = 1;
+	for (int i = 0; i < r; i++) {
+		d10 *= 10;
+	}
+
+	vector<int> c(10, 0); //счетчик цифр
+	vector<int> b(n); //временный массив
+	vector<int> d(10); //позиции для вставки
+
+	//подсчет кол-ва каждой цифры
+	for (int i = 0; i < n; i++) {
+		c[(a[i] / d10) % 10]++;
+	}
+
+	//вычисление стартовых позиций
+	d[0] = 0;
+	for (int i = 1; i < 10; i++) {
+		d[i] = d[i - 1] + c[i - 1];
+	}
+
+	//распределение элементов
+	for (int i = 0; i < n; i++) {
+		int x = (a[i] / d10) % 10;
+		b[d[x]] = a[i];
+		d[x]++;
+	}
+	a = b;
+}
+
+int main() {
+	//ввод кол-ва элементов
+	cout << "Введите кол-во элементов массива: ";
+	cin >> n;
+	a.resize(n);
+
+	//ввод элементов
+	cout << "Введите элементв массива: \n";
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	//находим максимальное число
+	int max = getMax();
+
+	//вычисляем кол-во разрядов
+	int r = 0;
+	while (max != 0) {
+		max /= 10;
+		r++;
+	}
+
+	//поразрядная сортировка 
+	for (int i = 0; i < r; i++) {
+		sortByDigit(i);
+	}
+
+	cout << "\nОтсортированный массив: \n";
+	for (int num : a) {
+		cout << num << " ";
+	}
+	return 0;
+}
